7206a4bc64c2605e506c8c4a02ca7d95a8c9a95d
Shane-Neeley <shane.neeley@gmail.com>
2017-02-22 14:49:13 -0800

    additional test scenarios and iterate obj list

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
-/**
- * Convert a mongodb query into an ES query.
- *
- * @param  {Object} q - the mongodb query
- * @return {Object} the ES query
- */
-
-
+var _ = require('underscore')
 
 module.exports = {
 
+    /**
+     * Convert a mongodb query into an ES query
+     * @param  {Object} q - the mongodb query
+     * @return {Object} the ES query
+     */
     // https://www.elastic.co/guide/en/elasticsearch/reference/current/query-filter-context.html
-    // maybe track the levels of the query for depth
-    // iterate over the depth
     convert: function(q) {
-        var es = {query: {}}
+        var es = {
+            query: {}
+        }
+
+        // http://stackoverflow.com/questions/15690706/recursively-looping-through-an-object-to-build-a-property-list
+        // recursively find the properties of this object
+        function iterate(obj, stack) {
+            for (var property in obj) {
+                if (obj.hasOwnProperty(property)) {
+                    if (typeof obj[property] == "object") {
+                        iterate(obj[property], stack + '.' + property);
+                    }
+                    else {
+                        props.push(stack + '.' + property)
+                    }
+                }
+            }
+        }
+
+        var props = []
+        iterate(q, '')
+        console.log(props)
+
         return es
     }
+
 };
--- test/index.js
+++ test/index.js
@@ -1,25 +1,45 @@
 var should = require('should')
-var mongo2es = require('../index')
-var convert = mongo2es.convert
+var mongo2es = require('../index').convert
 
 describe('When passed in a mongo query', function() {
 
     // basic query
     it('should convert it to an ES query', function() {
         var q = {name: "George"}
-        convert(q).should.deepEqual({query: {}})
+        mongo2es(q).should.deepEqual({query: {}})
     })
 
     // basic $and query
     it('should convert it to an ES query', function() {
         var q = {$and:[{name: "George"}, {lastname: "Greg"}]}
-        convert(q).should.deepEqual({query: {}})
+        mongo2es(q).should.deepEqual({query: {}})
     })
 
     // basic $or query
     it('should convert it to an ES query', function() {
         var q = {$or:[{name: "George"}, {lastname: "Greg"}]}
-        convert(q).should.deepEqual({query: {}})
+        mongo2es(q).should.deepEqual({query: {}})
+    })
+
+    // complex $and/$or query
+    it('should convert it to an ES query', function() {
+        var q = {
+            $and:[
+                {name: "George"},
+                {lastname: "Greg"},
+                {$or:[
+                    {name: 1},
+                    {lastname: true}
+                ]}
+            ]
+        }
+        mongo2es(q).should.deepEqual({query: {}})
+    })
+
+    // query into a subfield
+    it('should convert it to an ES query', function() {
+        var q = {"name.lastname": "George"}
+        mongo2es(q).should.deepEqual({query: {}})
     })
 
 })
