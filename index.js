972032e31a81d14907cf651c1f2d5ac244e13adc
Shane-Neeley <shane.neeley@gmail.com>
2017-02-22 11:49:09 -0800

    replaced chai with should.js

--- index.js
+++ index.js
@@ -4,9 +4,16 @@
  * @param  {Object} q - the mongodb query
  * @return {Object} the ES query
  */
+
+
+
 module.exports = {
+
+    // https://www.elastic.co/guide/en/elasticsearch/reference/current/query-filter-context.html
+    // maybe track the levels of the query for depth
+    // iterate over the depth
     convert: function(q) {
-        var es = {}
+        var es = {query: {}}
         return es
     }
 };
--- test/index.js
+++ test/index.js
@@ -1,10 +1,25 @@
-var should = require('chai').should(),
-    mongo2es = require('../index'),
-    convert = mongo2es.convert
+var should = require('should')
+var mongo2es = require('../index')
+var convert = mongo2es.convert
 
 describe('When passed in a mongo query', function() {
+
+    // basic query
     it('should convert it to an ES query', function() {
         var q = {name: "George"}
-        convert(q).should.equal({});
-    });
+        convert(q).should.deepEqual({query: {}})
+    })
+
+    // basic $and query
+    it('should convert it to an ES query', function() {
+        var q = {$and:[{name: "George"}, {lastname: "Greg"}]}
+        convert(q).should.deepEqual({query: {}})
+    })
+
+    // basic $or query
+    it('should convert it to an ES query', function() {
+        var q = {$or:[{name: "George"}, {lastname: "Greg"}]}
+        convert(q).should.deepEqual({query: {}})
+    })
+
 })
